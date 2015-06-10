d1617616a13549d82d305d26810c9984d56276e4
Birger <birger.nordmann@rwth-aachen.de>
2015-06-11 00:50:46 +0200

    Rewrote the slat_recognition script
    The script calculates a rotation error in the input image and reverses the rotation.
    The script implements all tested filters in single functions.

--- Python/slat_recognition/slat_recognition.py
+++ Python/slat_recognition/slat_recognition.py
@@ -13,121 +13,90 @@ if __name__ == '__main__':
     
     rectpoints = []
 
-    def perspective_transform(src_img, rectpoints):
-        img = src_img
-        src_quad = np.array([rectpoints[0], rectpoints[1], rectpoints[2], rectpoints[3]], np.float32);
-        dst_height =  np.max(np.asarray(rectpoints)[:,1]) - np.min(np.asarray(rectpoints)[:,1])
-        dst_width = np.max(np.asarray(rectpoints)[:,0]) - np.min(np.asarray(rectpoints)[:,0])
-        dst_quad = np.array([(0.0, 0.0), (dst_width, 0.0), (dst_width, dst_height), (0.0, dst_height)], np.float32);
-        transf_matr = cv2.getPerspectiveTransform(src_quad, dst_quad); # src, dst, 
-        transf_img = cv2.warpPerspective(img, transf_matr, (dst_width, dst_height));
-        print transf_matr
 
-        cv2.namedWindow("transformed", cv2.WINDOW_NORMAL);
-        cv2.imshow("transformed", transf_img);
-        cv2.destroyWindow('image')
-        cv2.destroyWindow('magnified')      
+    def print_straight_line(img, rho, angle, color = 255, thickness = 10):
+        height, width = img.shape
+        length = max(height, width) * 1.5
+        a = np.cos(angle)
+        b = np.sin(angle)
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + length*(-b))
+        y1 = int(y0 + length*(a))
+        x2 = int(x0 - length*(-b))
+        y2 = int(y0 - length*(a))
+        cv2.line(img,(x1,y1),(x2,y2),color, thickness)
+
+    def show_image(name, img):
+        cv2.namedWindow(name, cv2.WINDOW_NORMAL)
+        cv2.imshow(name, img)
+    
+    def erode_dilate(img, kernel = np.ones((4,2), np.uint8)):
+        opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+        return opening
         
-        #edge detection begin
-        img_gray = cv2.cvtColor(transf_img, cv2.COLOR_BGR2GRAY);
-
+    def custom_edge_detection(img):
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY);
         hist, bins = np.histogram(img_gray.flatten(), 256, [0, 256]);
-
         cdf = hist.cumsum()
         cdf_normalized = cdf * hist.max()/ cdf.max()
-
         hist_max_index = np.argmax(hist[25:200:1])
         hist_min_index = hist_max_index + np.argmin(hist[hist_max_index:hist_max_index + 50]);
-
         img_gray[np.where(img_gray > hist_min_index)] = 255
         img_gray[np.where(img_gray <= hist_min_index)] = 0
-
         img_sobel = cv2.Sobel(img_gray, cv2.CV_32F, 1, 0);
-
         img_edge = np.zeros(img_gray.shape, dtype = np.uint8)
         img_edge[np.where(img_sobel > 1000)] = 255
         img_edge[np.where(img_sobel < -1000)] = 255
-        
-        cv2.namedWindow("edge_detection", cv2.WINDOW_NORMAL)
-        cv2.imshow("edge_detection", img_edge)
-        #edge detection end -> img_edge
-        
-        #noise filter begin
-        kernel = np.ones((4,2), np.uint8);
-        opening = cv2.morphologyEx(img_edge, cv2.MORPH_OPEN, kernel)
-        
-        cv2.namedWindow("noise_filter", cv2.WINDOW_NORMAL)
-        cv2.imshow("noise_filter", opening)
-        #noise filter end -> opening
-        
-        #hough_transform begin
-        img = opening
-    
-        height, width = img.shape
-        img_wiped = np.zeros((height,width,3), np.uint8)
+        return img_edge
+
+    # max_angle is n radian
+    # returns the calculated angle shift
+    def hough_transform(img, minimum_vote, max_lines = 100, max_angle = 2*0.034906585039887):
+        height, width= img.shape
 
         for i in range(0, 12, 1):
             if(i%2 != 0):
                 img[i*int(height/12):(i+1)*int(height/12),:] = 0
-        
-        
-        img_wiped = img
-        lines = cv2.HoughLines(img_wiped,1,np.pi/180,300)
+
+        lines = cv2.HoughLines(img,1,np.pi/180,minimum_vote)
         print(len(lines))
         if(len(lines) > 100):
             lines = lines[0:100]
         good_lines = []
         for line in lines:
             for rho,theta in line:
-                if(theta < 0.034906585039887 * 2):
-                    a = np.cos(theta)
-                    b = np.sin(theta)
-                    x0 = a*rho
-                    y0 = b*rho
-                    x1 = int(x0 + 1000*(-b))
-                    y1 = int(y0 + 1000*(a))
-                    x2 = int(x0 - 1000*(-b))
-                    y2 = int(y0 - 1000*(a))
-
-                    #cv2.line(img_wiped,(x1,y1),(x2,y2),(0,0,255),2)
+                if(theta < max_angle):
+                    #print_straight_line(img, rho, theta)
                     good_lines.append((rho,theta))
+
         median_angle = good_lines[len(good_lines)/2][1]
         mean_angle = np.mean(np.asarray(good_lines)[:,1])
         
         print("Median line angle: " + str(median_angle))
         print("Mean line angle: " + str(mean_angle))
+        print_straight_line(img, width/2 - 50, median_angle)
+        print_straight_line(img, width/2 + 50, mean_angle)
+        return mean_angle
+
+    def rotate_image(img, angle):
+        img_shape = img.shape
+        rows = img_shape[0]
+        cols = img_shape[1]
+        rotation_matrix = cv2.getRotationMatrix2D((cols/2, rows/2), angle, 1.0);
+        print rotation_matrix
+        print img.shape
+        rotated_img = cv2.warpAffine(img, rotation_matrix, (cols, rows))
+        return rotated_img
         
-        rho = 1000
-        a = np.cos(median_angle)
-        b = np.sin(median_angle)
-        x0 = a*rho
-        y0 = b*rho
-        x1 = int(x0 + 10000*(-b))
-        y1 = int(y0 + 10000*(a))
-        x2 = int(x0 - 10000*(-b))
-        y2 = int(y0 - 10000*(a))
-        cv2.line(img_wiped,(x1,y1),(x2,y2),255,10)
-        rho = 2000
-        a = np.cos(mean_angle)
-        b = np.sin(mean_angle)
-        x0 = a*rho
-        y0 = b*rho
-        x1 = int(x0 + 10000*(-b))
-        y1 = int(y0 + 10000*(a))
-        x2 = int(x0 - 10000*(-b))
-        y2 = int(y0 - 10000*(a))
-        cv2.line(img_wiped,(x1,y1),(x2,y2),255,10)
-        
-        cv2.namedWindow("hough_lines", cv2.WINDOW_NORMAL)
-        cv2.imshow("hough_lines", img_wiped)
-        #hough_transform end ->
-        
-        
-        return
-        
-        #rotate img here with angle found by hough lines
-        
-        #
+    def perspective_transform(src_img, rectpoints):
+        src_quad = np.array([rectpoints[0], rectpoints[1], rectpoints[2], rectpoints[3]], np.float32);
+        dst_height =  np.max(np.asarray(rectpoints)[:,1]) - np.min(np.asarray(rectpoints)[:,1])
+        dst_width = np.max(np.asarray(rectpoints)[:,0]) - np.min(np.asarray(rectpoints)[:,0])
+        dst_quad = np.array([(0.0, 0.0), (dst_width, 0.0), (dst_width, dst_height), (0.0, dst_height)], np.float32);
+        transf_matr = cv2.getPerspectiveTransform(src_quad, dst_quad); # src, dst, 
+        transf_img = cv2.warpPerspective(img, transf_matr, (dst_width, dst_height));
+        return transf_img
         
         max_slat_width = 0.012137823 * dst_width # (big_slat_width)/(cube_width) : 0.31m / 25.54m
         offset_to_first_slat = 0.010571652 * dst_width # (offset_to_first_slat)/(cube_width) : 0.27m / 25.54m
@@ -175,7 +144,23 @@ if __name__ == '__main__':
 
         cv2.imshow("transformed", transf_img)
         print(slat_string)'''
-        
+
+    def slat_recognition(img, rectpoints):
+        transformed_img = perspective_transform(img, rectpoints)
+        show_image("Transformed:", transformed_img)
+
+        edge_img = custom_edge_detection(transformed_img)
+        show_image("Edgedetection:", edge_img)
+
+        opened_img = erode_dilate(edge_img) #Todo: custom kernel not working!
+        show_image("Opened:", opened_img)
+
+        mean_angle = hough_transform(opened_img, 300)
+        print(mean_angle)
+
+        rotated_img = rotate_image( opened_img, mean_angle * 180/np.pi);
+        show_image("Rotated:", rotated_img);
+
     def mouse_callback(event,x,y,flags,param):
         global img
         if event == cv2.EVENT_MOUSEMOVE:                
@@ -197,7 +182,8 @@ if __name__ == '__main__':
                 rectpoints.append((x,y))
                 print("Added point @ " + str(x) + "/" + str(y))
             if(len(rectpoints) == 4):
-                perspective_transform(img, rectpoints)
+                #perspective_transform(img, rectpoints)
+                slat_recognition(img, rectpoints)
     img = cv2.imread(fn, True)
     height, width, depth = img.shape
     cv2.namedWindow('image', cv2.WINDOW_NORMAL)
@@ -212,3 +198,4 @@ if __name__ == '__main__':
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
